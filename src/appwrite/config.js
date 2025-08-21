c68bfffe990b33e6094cc05451ffa04b8db5f6ec
parth-patel-jsk <parthp16625@gmail.com>
2025-08-21 20:47:36 +0530

    Made config file which has all services

---
+++ src/appwrite/config.js
@@ -0,0 +1,125 @@
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+import conf from "../conf/conf.js";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Apprwrite service :: createPost :: error, error");
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Apprwrite service :: updatePost :: error", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Apprwrite service :: deletePost :: error", error);
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Apprwrite service :: getPost :: error", error);
+      return false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Apprwrite service :: getPosts :: error", error);
+      return false;
+    }
+  }
+
+  //file upload method/service
+
+  async uploadFile(file){
+    try {
+        return await this.bucket.createFile(
+            conf.appwriteBucketId,
+            ID.unique(),
+            file
+        )
+    } catch (error) {
+        console.log("Apprwrite service :: uploadFile :: error", error);
+        return false
+    }
+  }
+
+  async deleteFile(fileld){
+    try {
+        return await this.bucket.deleteFile(
+            conf.appwriteBucketId,
+            fileld
+        )
+        return true
+    } catch (error) {
+        console.log("Apprwrite service :: deleteFile :: error", error);
+        return false
+    }
+  }
+
+}
+
+const service = new Service();
+
+export default service;
+//4:22:24 chai aur code
